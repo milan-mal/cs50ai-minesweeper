a02dca3ed71bdfd974a2c9feb0930d96f91fc5a8
Milan M <milanmalenak@gmail.com>
2024-03-24 20:13:56 +0100

    added methods in Sentence class

--- minesweeper.py
+++ minesweeper.py
@@ -105,27 +105,37 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if(len(self.cells) == self.count):
+            return self.cells
+        else:
+            return None
+            
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if(self.count == 0):
+            return self.cells
+        else:
+            return None
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if(cell in self.cells):
+            self.cells = self.cells - {cell}
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if(cell in self.cells):
+            self.cells = self.cells - {cell}
 
 
 class MinesweeperAI():
